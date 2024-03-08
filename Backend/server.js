de7a15f0e2cb6fa4c586536933a4dc2600d61ef2
P Vaibhav <pvaibhavofficial@gmail.com>
2024-03-08 23:52:09 +0530

    setting up backend

---
+++ Backend/server.js
@@ -0,0 +1,6 @@
+const express  = require ('express')
+const app = express();
+const PORT = process.env.PORT || 5000
+
+//start the server
+app.listen(PORT , console.log(`Server is running on PORT ${PORT}`));
